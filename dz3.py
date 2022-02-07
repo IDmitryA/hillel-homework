dd36fa3bffe5b3b78a9b61efc1024791d3040892
IDmitryA <ivan4ik1986@gmail.com>
2022-02-08 00:17:08 +0200

    add dz3.py

---
+++ dz3.py
@@ -0,0 +1,64 @@
+class Url:
+    def __init__(self, scheme=None, authority=None, path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __str__(self):
+        if self.path is not None:
+            p = '/' + '/'.join(self.path)
+        else:
+            p = ''
+        if self.query is not None:
+            l = []
+            for k, v in self.query.items():
+                l.append(f'{k}={v}')
+            q = '?' + '&'.join(l)
+        else:
+            q = ''
+        if self.fragment is not None:
+            f = f'#{self.fragment}'
+        else:
+            f = ''
+        return f'{self.scheme}://{self.authority}{p}{q}{f}'
+
+
+class HttpsUrl(Url):
+
+    def __init__(self, *args, **kwargs):
+        super(HttpsUrl, self).__init__(*args, **kwargs)
+        self.scheme = 'https'
+
+
+class HttpUrl(Url):
+
+    def __init__(self, *args, **kwargs):
+        super(HttpUrl, self).__init__(*args, **kwargs)
+        self.scheme = 'http'
+
+
+class GoogleUrl(HttpsUrl):
+
+    def __init__(self, *args, **kwargs):
+        super(GoogleUrl, self).__init__(*args, **kwargs)
+        self.authority = 'google.com'
+
+
+class WikiUrl(HttpsUrl):
+
+    def __init__(self, *args, **kwargs):
+        super(WikiUrl, self).__init__(*args, **kwargs)
+        self.authority = 'wikipedia.org'
+
+
+assert GoogleUrl() == HttpsUrl(authority='google.com')
+assert GoogleUrl() == Url(scheme='https', authority='google.com')
+assert GoogleUrl() == 'https://google.com'
+assert WikiUrl() == str(Url(scheme='https', authority='wikipedia.org'))
+assert WikiUrl(path=['wiki', 'python']) == 'https://wikipedia.org/wiki/python'
+assert GoogleUrl(query={'q': 'python', 'result': 'json'}) == 'https://google.com?q=python&result=json'
